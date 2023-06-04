e1bce461b3e91a29476e44b79c7a97d818c7364d
X <131550891+XBARAKUDA@users.noreply.github.com>
2023-06-04 23:29:36 +0100

    Create create_osint_directories.sh
    chmod +x create_osint_directories.sh
    ./create_osint_directories.sh

---
+++ create_osint_directories.sh
@@ -0,0 +1,118 @@
+#!/bin/bash
+
+# Create main directories
+mkdir -p OSINT
+cd OSINT
+
+# Create subdirectories for User Information
+mkdir "User Information"
+cd "User Information"
+mkdir Username
+mkdir "Email Address"
+cd ..
+
+# Create subdirectories for Domain Information
+mkdir "Domain Information"
+cd "Domain Information"
+mkdir "Domain Name"
+mkdir "IP Address"
+mkdir Geolocation
+mkdir "Host-Port Discovery"
+mkdir "Protected by Cloud Services"
+mkdir "Wireless Network info"
+mkdir "Network Analysis Tools"
+mkdir "IP Loggers"
+cd ..
+
+# Create subdirectories for Online Presence
+mkdir "Online Presence"
+cd "Online Presence"
+mkdir "Images-Videos-Docs"
+mkdir "Social Networks"
+mkdir "Instant Messaging"
+mkdir "People Search Engines"
+mkdir Dating
+mkdir "Telephone Numbers"
+mkdir "Public Records"
+mkdir "Business Records"
+cd ..
+
+# Create subdirectories for Company Information
+mkdir "Company Information"
+cd "Company Information"
+mkdir "Annual Reports"
+mkdir "General Info & News"
+mkdir "Company Profiles"
+mkdir "Employee Profiles & Resumes"
+mkdir Transportation
+cd ..
+
+# Create subdirectories for Geolocation Tools
+mkdir "Geolocation Tools"
+cd "Geolocation Tools"
+mkdir Maps
+mkdir "Geolocation Tools"
+cd ..
+
+# Create subdirectories for Web Search and Analysis
+mkdir "Web Search and Analysis"
+cd "Web Search and Analysis"
+mkdir "Search Engines"
+mkdir "Forums-Blogs-IRC"
+mkdir Archives
+mkdir "Language Translation"
+mkdir Metadata
+mkdir "Mobile Emulation"
+mkdir Terrorism
+mkdir "Dark Web"
+mkdir "Digital Currency"
+mkdir Classifieds
+cd ..
+
+# Create subdirectories for Encoding/Decoding Tools
+mkdir "Encoding-Decoding Tools"
+cd "Encoding-Decoding Tools"
+mkdir Tools
+cd ..
+
+# Create subdirectories for Malware Analysis
+mkdir "Malware Analysis"
+cd "Malware Analysis"
+mkdir "Malicious File Analysis"
+mkdir "OSINT Framework"
+mkdir "Exploits & Advisories"
+mkdir "Threat Intelligence"
+mkdir OpSec
+mkdir Documentation
+mkdir Training
+mkdir "Additional Resources"
+cd ..
+
+# Create subdirectories for Dark Web Investigation
+mkdir "Dark Web Investigation"
+cd "Dark Web Investigation"
+mkdir "TOR Search"
+mkdir "TOR Directories"
+mkdir OnionScan
+mkdir "Tor Scan"
+mkdir "Onion Investigator"
+mkdir "ACA Dark Web Investigation Support"
+mkdir "1BM X-Force Exchange"
+mkdir "OpenPhish.com"
+mkdir PhishTank
+mkdir PhishStats
+mkdir "Malware Information Sharing Platform"
+mkdir "Malware Patrol"
+mkdir "Project Honey Pot"
+mkdir "Cymon Open Threat Intelligence"
+mkdir "misecproject-combine"
+mkdir "hostintel-keithjones Github"
+mkdir "massive-octo-spice-csirtgadgets Github"
+mkdir "Bot Scout"
+mkdir "Bluell Threat Exchange (R)"
+mkdir APTROteS
+mkdir HoneyDB
+mkdir Pulsedive
+cd ..
+
+echo "Directories and subdirectories for OSINT mindmap have been created."
